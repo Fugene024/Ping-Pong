2128c328955188c6c10a8aea79a2dc5ab72551b0
Eugene <134151601+Fugene024@users.noreply.github.com>
2023-05-28 09:49:31 +0500

    main.py
    final version just for you >

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 ROCKET_IMG = 'Rocket.png'
 BALL_IMG = 'Ball.jpg'
 BACKGROUND_COLOR = (224, 255, 255)
+score = 0
 
 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -44,19 +45,27 @@ class Ball(Sprite):
     dy = 5
     def update(self, player_1, player_2):
         if self.rect.y <= 0:
-            dy *= -1
+            self.dy *= -1
         if self.rect.y >= (WINDOW_HEIGHT - self.rect.height):
-            dy *= -1
+            self.dy *= -1
         if self.rect.colliderect(player_1.rect):
-            dx *= -1
+            self.dx *= -1
         if self.rect.colliderect(player_2.rect):
-            dx *= -1
-        self.rect.x += dx
-        self.rect.y += dy
+            self.dx *= -1
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+    def is_outside(self):
+        if self.rect.x < -self.rect.width:
+            return 'left'
+        elif self.rect.x > WINDOW_WIDTH:
+            return 'right'
+        else:
+            return 'none'    
 
 
 player_left = Player(ROCKET_IMG, 20, 5, 40, 150, 5, pygame.K_w, pygame.K_s)
-player_right = Player(ROCKET_IMG, 740, WINDOW_HEIGHT-105, 40, 150, 5)
+player_right = Player(ROCKET_IMG, 740, WINDOW_HEIGHT-155, 40, 150, 5)
 ball = Ball(BALL_IMG, 250, 250, 50, 50)
 
 
@@ -68,11 +77,32 @@ while game_status != 'off':
         if event.type == pygame.QUIT:
             game_status = 'off'
     
-    ball.update(player_left, player_right)
-    player_left.update()
-    player_right.update()
-    ball.draw()
-    player_left.draw()
-    player_right.draw()
+    if game_status == 'game':
+        ball_status = ball.is_outside()
+        if ball_status == 'left':
+            game_status = 'result'
+            font = pygame.font.SysFont('Arial', 50)
+            text = font.render('Победил второй игрок! (R) ', True, (0, 0, 0))
+        elif ball_status == 'right':
+            game_status = 'result'
+            font = pygame.font.SysFont('Arial', 50)
+            text = font.render('Победил первый игрок! (R) ', True, (0, 0, 0))
+        ball.update(player_left, player_right)
+        player_left.update()
+        player_right.update()
+        ball.draw()
+        player_left.draw()
+        player_right.draw()
+    elif game_status == 'result':
+        rect = text.get_rect()
+        rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+        window.blit(text, (rect.x, rect.y))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_r]:
+            game_status = 'game'
+            player_left = Player(ROCKET_IMG, 20, 5, 40, 150, 5, pygame.K_w, pygame.K_s)
+            player_right = Player(ROCKET_IMG, 740, WINDOW_HEIGHT-155, 40, 150, 5)
+            ball = Ball(BALL_IMG, 250, 250, 50, 50)
+
     clock.tick(60)
     pygame.display.update()
